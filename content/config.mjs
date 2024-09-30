201e8d6e58d029375394ce15b8f0542b417136e6
rathermercurial <rathermercurial@protonmail.com>
2024-09-30 15:23:43 -0700

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,14 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+const config = {
+  title: "SuperBenefit Governance",
+  description: "Governance Documentation for SuperBenefit DAO",
+  author: "John",
+  logo: "/images/sb-icon.jpg",
+  domain: "https://sb-flowershow-instance.vercel.app/",
+  // links to the pages you want to link to in the navbar and in the footer
+  // navLinks: [{ href: "/about", name: "About" }],
+};
+
+export default config;
